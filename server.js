2578b3c08caad633e65b5d7a748206a5284dcad0
Keylane <keylanessilva07@gmail.com>
2021-08-18 12:09:44 -0300

    import routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const { ppid } = require('process');
 const mongoose = require('mongoose');
+const routes = require('./src/routes')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,10 +25,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-
-app.get('/', (req,res) => {
-    res.json({message: 'Hello World'});
-})
+app.use(routes);
+// app.use('/admin', routes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)

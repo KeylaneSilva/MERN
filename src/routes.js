2c06c24c462bd42b92c0f67a52986e7ae8e4c415
Keylane <keylanessilva07@gmail.com>
2021-08-18 23:04:50 -0300

    add routes

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ routes.get('/usuarios', Usuario.index);
 routes.get('/api/usuarios', Usuario.index);
 routes.post('/api/usuarios', Usuario.create);
 routes.get('/api/usuarios.details/:_id', Usuario.details)
+routes.delete('/api/usuarios.delete/:_id', Usuario.delete)
+routes.put('/api/usuarios.update', Usuario.update)
 
 module.exports = routes;
 

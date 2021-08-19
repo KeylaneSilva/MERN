c2f829be623aed6097bcfeb11658825c00e7371f
Keylane <keylanessilva07@gmail.com>
2021-08-19 11:29:49 -0300

    add routes produtos

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 const Usuario = require('./controllers/usuarios.controller')
+const Produto = require('./controllers/produtos.controller')
 
 routes.get('/usuarios', Usuario.index);
 
@@ -9,9 +10,16 @@ routes.get('/usuarios', Usuario.index);
 // Rotas Usuário
 routes.get('/api/usuarios', Usuario.index);
 routes.post('/api/usuarios', Usuario.create);
-routes.get('/api/usuarios.details/:_id', Usuario.details)
-routes.delete('/api/usuarios.delete/:_id', Usuario.delete)
-routes.put('/api/usuarios.update', Usuario.update)
+routes.get('/api/usuarios.details/:_id', Usuario.details);
+routes.delete('/api/usuarios.delete/:_id', Usuario.delete);
+routes.put('/api/usuarios.update', Usuario.update);
+
+// Rotas Produtos
+routes.get('/api/produtos', Produto.index);
+routes.post('/api/produtos', Produto.create);
+routes.get('/api/produtos.details/:_id', Produto.details);
+routes.delete('/api/produtos.delete/:_id', Produto.delete);
+routes.put('/api/produtos.update', Produto.update);
 
 module.exports = routes;
 

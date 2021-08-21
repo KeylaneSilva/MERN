0990366de3eff1bf51ff2c349ef0b8e5976902c9
Keylane <keylanessilva07@gmail.com>
2021-08-21 11:51:37 -0300

    formulario de cad usuarios

--- client/src/pages/admin/usuarios/usuarios.cadastrar.js
+++ client/src/pages/admin/usuarios/usuarios.cadastrar.js
@@ -1,9 +1,123 @@
-import React from "react";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function UsuariosCadastrar() {
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import MenuAdmin from '../../../components/menu-admin';
+import Footer from '../../../components/footer-admim';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: 30,
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  formControl:{
+    width: '100%'
+  }
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+  
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   return (
-    <div><h1>Cadastro de Usuarios</h1></div>
+    <div className={classes.root}> 
+    <MenuAdmin title={'USUÁRIOS'}/>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item sm={12}>
+              <Paper className={classes.paper}>
+                <h2>Cadastro de Usuários</h2>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={12}>
+                    <TextField
+                      required
+                      id="nome"
+                      name="nome"
+                      label="Nome completo"
+                      fullWidth
+                      autoComplete="given-name"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      type="email"
+                      id="email"
+                      name="email"
+                      label="Email"
+                      fullWidth
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControl 
+                      required 
+                      className={classes.formControl}
+                    >
+                      <InputLabel id="demo-simple-select-label">Tipo</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={1}
+                        // onChange={handleChange}
+                      >
+                        <MenuItem value={1}>Administrador</MenuItem>
+                        <MenuItem value={2}>Usuário</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <TextField
+                      required
+                      type="password"
+                      id="senha"
+                      name="senha"
+                      label="Senha"
+                      fullWidth
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Footer />
+          </Box>
+        </Container>
+      </main>
+    </div>
   );
 }
-
-
